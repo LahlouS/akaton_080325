c5ff99243a32aebaa310337d6b14eb585e9bd986
Garance Colomer <gcolomer@protonmail.com>
2025-03-08 12:33:10 +0100

    refactor: main.py

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ import os
 from dotenv import load_dotenv
 import goodfire
 
-# Load .env file
-load_dotenv()
 
-# Get values from .env
-api_key = os.getenv("GOODFIRE_API_KEY")
+def main():
+    # Get values from .env
+    api_key = os.getenv("GOODFIRE_API_KEY")
 
-client = goodfire.Client(api_key=api_key)
-# Instantiate a model variant.
-variant = goodfire.Variant("meta-llama/Llama-3.3-70B-Instruct")
+    client = goodfire.Client(api_key=api_key)
+    # Instantiate a model variant.
+    variant = goodfire.Variant("meta-llama/Llama-3.3-70B-Instruct")
 
-for token in client.chat.completions.create(
-    [{"role": "user", "content": "Hi, how are you?"}],
-    model=variant,
-    stream=True,
-    max_completion_tokens=100,
-):
-	print(token.choices[0].delta.content, end="")
+    for token in client.chat.completions.create(
+        [{"role": "user", "content": "Hi, how are you?"}],
+        model=variant,
+        stream=True,
+        max_completion_tokens=100,
+    ):
+        print(token.choices[0].delta.content, end="")
+
+if __name__ == "__main__":
+    # Load .env file
+    load_dotenv()
+    main()
