7e9f87152e934408efe7242e945bb344cdb04e36
lchan <luc92.chan@gmail.com>
2025-03-08 17:17:14 +0100

    reformat pipeline

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from prompt import build_prompt
 from parse_csv import parse_csv
 import pandas as pd
 
-
 def main():
     api_key = os.getenv("GOODFIRE_API_KEY")
 
@@ -15,17 +14,46 @@ def main():
     data_frame = parse_csv()
     prompt_list = build_prompt(data_frame=data_frame)
     llm_answers: list[str] = []
-    for prompt in prompt_list:
-        for token in client.chat.completions.create(
-            [{"role": "user", "content": prompt}],
-            model=variant,
-            stream=True,
-            max_completion_tokens=3,
-        ):
-            answer: str = token.choices[0].delta.content
-            llm_answers.append(answer)
-    data_frame["vote_prediction"] = llm_answers
+
+    for index, prompt in enumerate(prompt_list):
+        tokens = list(client.chat.completions.create(
+        [{"role": "user", "content": prompt}],
+        model=variant,
+        stream=True,
+        max_completion_tokens=100,
+        ))
+        word = ''
+        for token in tokens:
+            word += token.choices[0].delta.content
+        llm_answers.append(word)
+
+        if index == 2: break
     print(llm_answers)
+
+        # for token in client.chat.completions.create(
+        # [{"role": "user", "content": prompt}],
+        # model=variant,
+        # stream=True,
+        # max_completion_tokens=100,
+        # ):
+        #     print(token.choices[0].delta.content, end="")
+
+
+    # for prompt in prompt_list:
+    #     message = [{"role": "user", "content": prompt}],
+    #     response = client.chat.completions.create(
+    #         message,
+    #         model=variant,
+    #         stream=True,
+    #         max_completion_tokens=3,
+    #     )
+    #     for token in response:
+    #         print(token.choices[0].delta.content, end="")
+
+            #answer: str = token.choices[0].delta.content
+            #llm_answers.append(answer)
+    #data_frame["vote_prediction"] = llm_answers
+    #print(llm_answers)
 
 if __name__ == "__main__":
     # Load .env file
--- prompt.py
+++ prompt.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from dotenv import load_dotenv
-
 
 PROMPT_TEMPLATE="""You are a political analyst. Your goal is to determine who the user voted for between Trump and Biden.
 To do this, you must rely on the user's name, their description, their tweet, and their state.
