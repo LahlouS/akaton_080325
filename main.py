f8c3d171f8957d0ac93e34d31f4ec1da3d5d0f06
lchan <luc92.chan@gmail.com>
2025-03-08 12:26:25 +0100

    add prompt creation example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import goodfire
 
 # Load .env file
 load_dotenv()
@@ -7,5 +8,14 @@ load_dotenv()
 # Get values from .env
 api_key = os.getenv("GOODFIRE_API_KEY")
 
-# Print to verify (remove this in production)
-print(f"API Key: {api_key}")
+client = goodfire.Client(api_key=api_key)
+# Instantiate a model variant.
+variant = goodfire.Variant("meta-llama/Llama-3.3-70B-Instruct")
+
+for token in client.chat.completions.create(
+    [{"role": "user", "content": "Hi, how are you?"}],
+    model=variant,
+    stream=True,
+    max_completion_tokens=100,
+):
+	print(token.choices[0].delta.content, end="")
